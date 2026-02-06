1aafc333f117e60541881c1819993b1bc3ebfd3f
Jigoogle <allentripathi.2529@gmail.com>
2026-02-06 15:59:46 +0530

    DSA STUDYING WITH CPP LANGUAGE TO MASTER ITS BASICS

---
+++ code.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+// namespace std
+
+//This this starting point of Code .
+// int main() {
+//     int a = 11, b = 5;
+//     cout << "sum =" <<(a+b)<<endl;
+//     cout <<"difference =" << (a-b) << endl;
+//     cout <<"product = " << (a+b)<< endl;
+//     cout << "division =" << (a/b) << endl;  //2
+//     cout << "Modulo= " << (a%b) << endl;
+//     return 0;
+// }
+//Boilerplate Code -
+//////////////////////////////////////
+//Checking Is is vote or not 
+
+// int main(){
+//     int age;
+//     cout<<"enter age: ";
+//     cin >> age;
+
+//     if (age>=18) {
+//         cout << "You Can vote \n";
+//     }else{
+//         cout <<"You can't vote\n";
+//     }
+//     return 0;
+// }
+
+///IF else // 
+// int main(){
+//     int n;
+//     cout << "enter Number ";
+//     cin >> n;
+
+//     if(n%2 ==0){
+//         cout<<"even \n";
+//         cout<<"bye\n";
+
+//     }else{
+//         cout<<"odd\n";
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int marks;
+//     cout <<"enter a marks";
+//     cin >>marks;
+
+//     if(marks >=90){
+//         cout << "A\n";
+//     }else if(marks >= 80 &&  90) {
+//         cout << "B\n";
+//     } else {
+
+//         cout << "C\n";
+//     }
+    
+//     return 0;
+// }
+
+
